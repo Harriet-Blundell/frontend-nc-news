488657a91ddca1ba1574ff22c93602547ef3e568
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-03-20 21:00:28 +0000

    create a react app for front-end NC News website and an api.js file

---
+++ api.js
@@ -0,0 +1,63 @@
+import axios from 'axios';
+
+const NCNews = axios.create({
+  baseURL: 'https://harrietblundell-be-nc-news.herokuapp.com/api/'
+});
+
+export const fetchAllTopics = () => {
+  return NCNews.get('topics').then(({ data }) => {
+    return data;
+  });
+};
+
+export const fetchAllArticles = query => {
+  return NCNews.get('articles', {
+    params: query
+  }).then(({ data }) => {
+    return data;
+  });
+};
+
+export const fetchArticleById = id => {
+  return NCNews.get(`articles/${id}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const fetchCommentsByArticleId = id => {
+  return NCNews.get(`articles/${id}/comments`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const postCommentById = (id, commentBody) => {
+  return NCNews.post(`articles/${id}/comments`, commentBody).then(
+    ({ data }) => {
+      return data;
+    }
+  );
+};
+
+export const fetchUsers = () => {
+  return NCNews.get(`users`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const deleteCommentById = id => {
+  return NCNews.delete(`comments/${id}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const deleteArticleById = id => {
+  return NCNews.delete(`articles/${id}`).then(({ data }) => {
+    return data;
+  });
+};
+
+export const patchVote = (id, vote, type) => {
+  return NCNews.patch(`${type}/${id}`, { inc_votes: vote }).then(({ data }) => {
+    return data;
+  });
+};
