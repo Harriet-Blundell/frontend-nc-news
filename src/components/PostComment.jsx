5bd17d0f1601137ac119f45a07c62cf5bcc24346
Harriet-Blundell <harriet.blundell27@hotmail.co.uk>
2020-04-23 18:43:52 +0100

    this commit will add media queries to the single article component

--- src/components/PostComment.jsx
+++ src/components/PostComment.jsx
@@ -1,36 +1,37 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
 class PostedComment extends Component {
   state = {
-    body: ''
-  };
+    body: '',
+  }
 
-  handleSubmit = event => {
-    event.preventDefault();
-    const { body } = this.state;
-    const { username, postNewComment } = this.props;
-    postNewComment({ username, body });
+  handleSubmit = (event) => {
+    event.preventDefault()
+    const { body } = this.state
+    const { username, postNewComment } = this.props
+    postNewComment({ username, body })
     this.setState({
-      body: ''
-    });
-  };
+      body: '',
+    })
+  }
 
   handleChange = (text, key) => {
     this.setState({
-      body: text
-    });
-  };
+      body: text,
+    })
+  }
 
   render() {
     return (
-      <div>
-        <form onSubmit={this.handleSubmit}>
+      <div className='commentsContainer'>
+        <form onSubmit={this.handleSubmit} className='commentsForm'>
           <textarea
-            rows="7"
-            cols="50"
-            placeholder="Post a comment here..."
-            onChange={event => {
-              this.handleChange(event.target.value, 'body');
+            className='commentTextArea'
+            rows='7'
+            cols='50'
+            placeholder='Post a comment here...'
+            onChange={(event) => {
+              this.handleChange(event.target.value, 'body')
             }}
             value={this.state.body}
             required
@@ -39,8 +40,8 @@ class PostedComment extends Component {
           <button>Submit</button>
         </form>
       </div>
-    );
+    )
   }
 }
 
-export default PostedComment;
+export default PostedComment
--- src/components/SingleArticle.jsx
+++ src/components/SingleArticle.jsx
@@ -1,31 +1,32 @@
-import React, { Component } from 'react';
-import { fetchArticleById } from '../api';
-import Voter from './Voter';
-import CommentsList from './CommentsList';
-import ErrorPage from './ErrorPage';
-import { formatDate } from '../utils';
+import React, { Component } from 'react'
+import { fetchArticleById } from '../api'
+import Voter from './Voter'
+import CommentsList from './CommentsList'
+import ErrorPage from './ErrorPage'
+import { formatDate } from '../utils'
+import './ArticleCard.css'
 
 class SingleArticle extends Component {
   state = {
     article: [],
     isLoading: true,
-    error: null
-  };
+    error: null,
+  }
 
   componentDidMount() {
     fetchArticleById(this.props.id)
       .then(({ article }) => {
         this.setState({
           article: article,
-          isLoading: false
-        });
+          isLoading: false,
+        })
       })
-      .catch(err => {
+      .catch((err) => {
         this.setState({
           error: { msg: err.response.data.msg, status: err.response.status },
-          isLoading: false
-        });
-      });
+          isLoading: false,
+        })
+      })
   }
 
   componentDidUpdate(prevProp, prevState) {
@@ -33,37 +34,37 @@ class SingleArticle extends Component {
       fetchArticleById(this.props.id).then(({ article }) => {
         return {
           article: article,
-          isLoading: false
-        };
-      });
+          isLoading: false,
+        }
+      })
     }
   }
 
   render() {
-    const { article, isLoading, error } = this.state;
+    const { article, isLoading, error } = this.state
 
     if (isLoading) {
-      return <p>Loading article...</p>;
+      return <p>Loading article...</p>
     }
 
     if (error) {
-      return <ErrorPage error={error} />;
+      return <ErrorPage error={error} />
     }
 
     return (
-      <div className="singleArticleById">
+      <div className='singleArticleById'>
         <h2>Article:</h2>
 
         <h3>{article.title}</h3>
-        <p className="singleArticle_generalinfo">General Information:</p>
-        <p className="singleArticleBody">{article.body}</p>
+        <p className='singleArticle_generalinfo'>General Information:</p>
+        <p className='singleArticleBody'>{article.body}</p>
         <Voter
           currentVote={article.votes}
           id={article.article_id}
           type={'articles'}
         />
         <p>Topic: {article.topic}</p>
-        <p className="postedTag">Posted by: {article.author}</p>
+        <p>Posted by: {article.author}</p>
         <p>Date: {formatDate(article.created_at)}</p>
         <p>Comments: {article.comment_count}</p>
 
@@ -72,8 +73,8 @@ class SingleArticle extends Component {
           username={this.props.username}
         />
       </div>
-    );
+    )
   }
 }
 
-export default SingleArticle;
+export default SingleArticle
